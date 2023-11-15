3d009a952ded33d61cc1c3ac89237c6f6f17e234
debra <mumoantony00@gmail.com>
2023-11-15 22:39:44 +0300

    latest

--- src/containers/navbar/App.jsx
+++ src/containers/navbar/App.jsx
@@ -1,21 +1,18 @@
-/* eslint-disable jsx-a11y/alt-text */
-import {useEffect, useRef, useState} from 'react';
-import {Route, BrowserRouter as Router, Switch} from 'react-router-dom';
-import {HashLink as Link} from 'react-router-hash-link';
+import React, { useEffect, useRef, useState } from 'react';
+import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
 import styled from 'styled-components';
 import HomePage from '../Home/HomePage';
 import logoClose from './assets/ham-c.svg';
 import hamLogo from './assets/ham.svg';
 import Logo from './assets/logo.png';
 import './styles.scss';
-
-import {MdxContent} from '../Mdx';
+import { MdxContent } from '../Mdx';
 
 const NAVIGATION_OFFSET = 66;
 
 const Wrapper = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
 
   @media (max-width: 1000px) {
     margin: 0;
@@ -30,7 +27,7 @@ const Wrapper = styled.div`
       flex-direction: row;
       width: 100%;
       height: 55%;
-      background-color: #01bf71;
+      background-color: #121930;
     }
   }
 `;
@@ -46,54 +43,66 @@ const NavLink = styled.li`
   padding: 0 1rem;
 `;
 
-const NavLinkWrapper = styled.div``;
+const NavLinkWrapper = styled.div`
+@media (max-width: 1000px) {
+  display: ${props => (props.toggle ? 'none' : 'static')};
+  height: 100vh;
+  width: 100vw;
+  position: fixed;
+  top: ${props => (props.toggle ? '-1000px' : '0px')};
+  transition: top 1s;
+  justify-content: space-between;
+  .nav-content {
+    flex-direction: row;
+    width: 100%;
+    height: 55%;
+    background-color: #121930;
+  }
+}
+`;
 
 const ButtonWrapper = styled.div`
-  gap: 1rem;
+gap: 1rem;
 
-  @media (max-width: 1000px) {
-    flex-direction: column;
-    background-color: #fff;
-  }
+@media (max-width: 1000px) {
+  flex-direction: column;
+  background-color: #fff;
+}
 `;
 
-const Button = styled.li`
-  width: 10rem;
-  color: #010606;
-  padding: 5px 15px;
-  text-align: center;
-  background-color: #ffe286;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  font-size: 16px;
-  transition: background-color 0.3s ease;
-  &:hover {
-    background-color: #fff;
-  }
+const Button = styled.div`
+width: 10rem;
+color: #010606;
+padding: 5px 15px;
+text-align: center;
+background-color: #ffe286;
+border: none;
+border-radius: 4px;
+cursor: pointer;
+font-size: 16px;
+transition: background-color 0.3s ease;
+&:hover {
+  background-color: #fff;
+}
 `;
 
-const Navbar = () => {
+const Navigation = ({ isOffset }) => {
   const [toggle, setToggle] = useState(true);
-  const [isOffset, setIsOffset] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(isOffset);
 
-  const navigation = useRef();
+  const navigationRef = useRef();
 
-  const listenScrollEvent = e => {
-    if (window.scrollY >= NAVIGATION_OFFSET) {
-      setIsOffset(true);
-    } else {
-      setIsOffset(false);
-    }
+  const handleScroll = () => {
+    setIsScrolled(window.scrollY >= NAVIGATION_OFFSET);
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', listenScrollEvent);
-    return () => window.removeEventListener('scroll', listenScrollEvent);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  const handleOutsideCick = (event, ref) => {
-    if (!ref.current.contains(event.target)) {
+  const handleOutsideClick = (event) => {
+    if (!navigationRef.current.contains(event.target)) {
       setToggle(true);
     } else {
       setToggle(false);
@@ -101,24 +110,25 @@ const Navbar = () => {
   };
 
   useEffect(() => {
-    document.addEventListener('mousedown', e =>
-      handleOutsideCick(e, navigation)
-    );
-
+    document.addEventListener('mousedown', handleOutsideClick);
     return () => {
-      document.removeEventListener('mousedown', e =>
-        handleOutsideCick(e, navigation)
-      );
+      document.removeEventListener('mousedown', handleOutsideClick);
     };
   }, []);
 
+  const handleSignIn = () => {
+    // Implement sign-in logic here
+    // For example: redirect to a sign-in page or trigger a sign-in modal
+    console.log('Sign in clicked');
+  };
+
   return (
     <Router>
-      <nav className={`nav_bar ${isOffset && 'nav_bar-offset-crossed'}`}>
+      <nav className={`nav_bar ${isScrolled && 'nav_bar-offset-crossed'}`}>
         <Wrapper toggle={toggle}>
-          <NavLinkContainer className="nav-content" ref={navigation}>
+          <NavLinkContainer className="nav-content" ref={navigationRef}>
             <NavLinkWrapper>
-              <NavLink>
+            <NavLink>
                 <Link to={`#home`}>
                   <img
                     src={Logo}
@@ -158,28 +168,36 @@ const Navbar = () => {
               </NavLink>
             </NavLinkWrapper>
             <ButtonWrapper>
-              <Link to={`#signin`}>
-                <Button className="links">SignIn </Button>{' '}
-              </Link>
+              {/* Sign In button */}
+              {/* <Link to="#signin"> */}
+                <Button className="links" onClick={handleSignIn}>
+                  SignIn
+                </Button>
+              {/* </Link> */}
             </ButtonWrapper>
+            {/* Close button */}
             <li>
               <img
                 className="s-close"
                 onClick={() => setToggle(true)}
                 src={logoClose}
+                alt="Close"
               />
             </li>
           </NavLinkContainer>
           <div className="ease" />
         </Wrapper>
 
+        {/* Ham logo for mobile */}
         <img
           className="s-open"
           onClick={() => setToggle(false)}
           src={hamLogo}
+          alt="Open"
         />
       </nav>
 
+      {/* Switch and Routes */}
       <Switch>
         <Route path="/blog" exact={true}>
           <MdxContent />
@@ -190,6 +208,21 @@ const Navbar = () => {
       </Switch>
     </Router>
   );
+};
+
+const Navbar = () => {
+  const [isOffset, setIsOffset] = useState(false);
+
+  const listenScrollEvent = () => {
+    setIsOffset(window.scrollY >= NAVIGATION_OFFSET);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', listenScrollEvent);
+    return () => window.removeEventListener('scroll', listenScrollEvent);
+  }, []);
+
+  return <Navigation isOffset={isOffset} />;
 };
 
 export default Navbar;
